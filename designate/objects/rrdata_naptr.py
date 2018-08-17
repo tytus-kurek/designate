85b31b1be9e01c3776d5fb7e216598a3455ffa2a
Tytus Kurek <tytus.kurek@canonical.com>
2018-08-17 13:05:09 +0200

    Copyright statements update
    This patchset updates copyright statements.

--- designate/objects/rrdata_naptr.py
+++ designate/objects/rrdata_naptr.py
@@ -1,5 +1,6 @@
-# Copyright (c) 2014 Rackspace Hosting
-# All Rights Reserved.
+# Copyright 2018 Canonical Ltd.
+#
+# Author: Tytus Kurek <tytus.kurek@canonical.com>
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
@@ -12,6 +13,7 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
+
 from designate.objects.record import Record
 from designate.objects.record import RecordList
 from designate.objects import base
